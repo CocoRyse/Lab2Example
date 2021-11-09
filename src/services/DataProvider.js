ceaa8232313302204f6397c5f99e83d6de5da302
Виктор Решетов <vityareshetov@yandex.com>
2021-11-09 19:53:41 +0300

    Обновил зависимости

--- src/services/DataProvider.js
+++ src/services/DataProvider.js
@@ -1,23 +1,5 @@
 import { Books } from './data/Books';
 
-const books = Books();
-
-const DataProvider = () => {
-  const getAllBooks = () => {};
-
-  const getBookByID = () => {};
-
-  const updateBook = () => {};
-
-  const putBook = () => {};
-
-  const deleteBook = () => {};
-
-  return {
-    getAllBooks,
-    getBookByID,
-    updateBook,
-    putBook,
-    deleteBook,
-  };
-};
+export class DataProvider {
+  static books = Books();
+}
